4b7360ff24797a88e6e03c253cbdb6a1303f9dde
Harold Putman <h.putman@ieee.org>
2023-06-17 14:52:02 -0400

    Add art show page.

---
+++ resources/views/web/art-gallery/rhythm-show.blade.php
@@ -0,0 +1,68 @@
+@extends('web.layouts.master')
+
+@section('title', 'Rhythm')
+
+@section('content')
+  <div class="mx-auto w-full md:w-3/4 px-6">
+    <header class="mx-auto max-w-3xl md:text-center px-2">
+        <h1 class="mt-4 text-4xl font-bold tracking-tight text-black sm:text-4xl">
+            Rhythm</h1>
+        <p class="text-xl mt-1 text-gray-dark tracking-wide">The inaugural at the Deanne Moore Gallery and the  Grand Opening of Ensembles</>
+    </header>
+    <article>
+    <h2 class="mt-4 font-bold text-black text-2xl">Show Type</h2>
+    <p class="ml-2 mb-2">Open</p>
+    <h2 class="mt-4 font-bold text-black text-2xl">Show Title</h2>
+    <p class="ml-2 mb-2">Rhythm</p>
+    <h2 class="mt-4 font-bold text-black text-2xl">Entry Fee</h2>
+    <p class="ml-2 mb-2">A $10 donation is suggested but not required</p>
+    <h2 class="mt-4 font-bold text-black text-2xl">Media</h2>
+    <p class="ml-2 mb-2">Painting, watercolor, printmaking, mixed media, drawing, pastel, encaustic,
+        fiber, and photography. The Deanne Moore Gallery accepts 2-dimensional,
+        wall-hung works only.</p>
+    <h2 class="mt-4 font-bold text-black text-2xl">Show Statement</h2>
+    <p class="ml-2 mt-2">The Deanne Moore Gallery invites artists to submit up to three (3) artworks for consideration in the Inaugural Exhibition of the Ensembles Center, Rhythm. </p>
+    <p class="ml-2 mt-2">
+        <strong class="font-bold">When we hear the word rhythm, we think of music. Beats. Tempos. Melodies. Rhythms that move us.</strong>
+             Rhythm is, in fact, originally a music term that means movement patterns of musical notes and lyrics at a certain time. But rhythm also exists in nature as regularly recurrent changes as in seasons or in biological processes. Rhythm can exist in poetry as metre, a pattern of stressed and unstressed parts of words or alternations of strong and weak elements in speech.</p>
+    <p class="ml-2 mt-2">There are various definitions of the word “rhythm.”  According to the Merriam-Webster Online Dictionary, rhythm is defined as “movement, fluctuation, or variation marked by the regular recurrence or natural flow of related elements”.</p>
+    <p class="ml-2 mt-2">How does rhythm exist in your art? As geometrically structured patterns of lines or shapes?  Or do undulating, swirling, organic forms create rhythm and movement in your work? Would you describe your work as having hyper-energized rhythm or a calming fluid pulse?</p>
+    <p class="ml-2 mt-2">Artists are encouraged to present contemporary works that celebrate rhythm in their own unique style to represent the diverse community we live in.</p>
+    <h2 class="mt-4 font-bold text-black text-2xl">Requirements</h2>
+    <p class="ml-2 mt-2"><strong class="font-bold">The deadline to submit to this exhibit is June 30th, 2023 by 11:59pm.</strong></p>
+    <p class="ml-2 mt-2">Entries are limited to a maximum of three and must have been created in
+       the last two years.</p>
+    <p class="ml-2 mt-2">Hanging work may not exceed 25 lbs. per unit and are limited to 24” (including frame) in either direction.
+    <p class="ml-2 mt-2">
+        Delivery of Works: Artwork must be at the Deanne Moore Gallery, Charlestown, IN by July 24, 2023.
+        Exhibition Dates: Aug 1 - Sep 10, 2023.
+        <i class="italic">Exact location will be confirmed in acceptance letter.</i>
+    </p>
+    <h2 class="mt-4 font-bold text-black text-2xl">Guidelines</h2>
+    <ul class="ml-2 mt-2 list-disc px-4">
+        <li>Artworks must be available for loan during the entire exhibition period.</p>
+        <li>Accepted artwork must be hand delivered, installation ready unless otherwise noted in the acceptance letter.</li>
+        <li>Our standard for 2D installation is a hanging wire or cleat. NO sawtooth hangers.</li>
+        <li>All hanging hardware must be included along with detailed hanging instructions for any non-standard artwork accepted.</li>
+        <li>The Deanne Moore Gallery reserves the right to reject accepted work that differs significantly from the submitted images or is inadequately prepared for installation.</li>
+        <li>Each artwork must have a label attached on the back or bottom of the pieces containing the following information: Artist’s name, title of piece, media, date of completion, sale price (or if NFS, the insurance value).</li>
+        <li>Artist must self-insure artwork for the duration of the loaned period, including the date of arrival at the center through the stated pick-up date.</li>
+        <li>The Deanne Moore Gallery suggests a voluntary 10% commission on all sales made because of the exhibition to be made to the Ensembles Center.</li>
+        <li>The Deanne Moore Gallery reserves the right to use images of the artworks for educational and promotional purposes including but not limited to newspaper, television, radio, social media, and the Internet.</li>
+        <li>Upon acceptance artists may be asked to submit a high-resolution image of the accepted artwork for such purposes.</li>
+    </ul>
+    <h2 class="mt-4 font-bold text-black text-2xl">Timeline</h2>
+    <ul class="ml-2 mt-2 list-disc px-4">
+        <li>Submission deadline: June 30, 2023</li>
+        <li>Notification of Acceptance: July 14, 2023</li>
+        <li>All work must be dropped off by July 24, 2023</li>
+        <li>Show dates: August 1 - September 10, 2023</li>
+        <li>Artist Reception: TBA</li>
+    </ul>
+    <h2 class="mt-4 font-bold text-black text-2xl">Contact</h2>
+    <p class="ml-2 mt-2">
+        For more information or to register, email Jenny at <a class="hover:underline" href="mailto:art@charlestownensembles.com">art@charlestownensembles.com</a>
+    </p>
+    </article>
+  </div>
+@stop
