e0296fb24fb415b467aa3ab764d44de48e9ae7e7
Harold Putman <h.putman@ieee.org>
2024-03-10 20:12:10 -0400

    Add facebook link to the footer

--- resources/views/web/layouts/footer.blade.php
+++ resources/views/web/layouts/footer.blade.php
@@ -1,7 +1,9 @@
 <footer class="w-full">
-  <div class="mx-auto max-w-7xl w-full px-6 border-gray-mist  border-solid border-t">
+  <div class="mx-auto max-w-7xl w-full px-6 border-gray-mist  border-solid border-t mt-3">
+    <div class="my-2 py-2 text-center text-gray text-sm">
+        <a href="https://facebook.com/charlestownensembles" target="_blank" class="text-gray">Facebook</a>
+    </div>
     <div class="my-2 py-2 text-center text-gray text-xs">
         Ensembles, Inc. is a registered 501(c)(3) organization based in Charlestown, IN.
     </div>
-  </div>
 </footer>
