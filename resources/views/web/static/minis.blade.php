7fe52432f1694eba33a68f78147e727ce31526c9
Harold Putman <h.putman@ieee.org>
2025-04-19 10:56:03 -0400

    Update minis.blade.php

--- resources/views/web/static/minis.blade.php
+++ resources/views/web/static/minis.blade.php
@@ -11,18 +11,13 @@
 <img src="{{ asset('image/hero/mini-musicians.jpg') }}"
 alt="Mini Musicians"
 loading="eager">
-    <p class="mt-4 text-lg text-center">Next session classes start the week of April 14.</p>
+    <p class="mt-4 text-lg text-center">Next session classes start in August.</p>
     <div class="max-w-4xl mx-auto py-12 px-4">
         <h2 class="text-3xl font-bold text-center mb-8">Choose your class</h2>
         <div class="grid md:grid-cols-2 gap-8">
-            <!-- Thursday Button -->
-            <a href="{{ route('register', ['classId' => 208]) }}"
-               class="inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-primary-dark  mb-2">
-                <span class="text-xl font-bold">Thursdays 10AM</span>
-            </a>
 
             <!-- Friday Button -->
-            <a href="{{ route('register', ['classId' => 204]) }}"
+            <a href="{{ route('register', ['classId' => 208]) }}"
                class="inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-primary px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-primary-dark mb-2">
                 <span class="text-xl font-bold">Fridays 10AM</span>
             </a>
